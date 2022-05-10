14ad9a288cbdb4315fbb72a0d3d9f8a53143eba0
godspower <geefeelix@gmail.com>
2022-05-10 11:19:20 -0400

    commit tree 62059aa16bf58ac5809409ce8cdcdf02a7de88bd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+  let i = 0;
+  while (i < cards.length) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    i++
+  }
+  return cards;
+}
+ 
+
+
+console.log(writeCards(names));
+
+function countDown(cards){
+  let i = 0;
+  for (let countDown = 0; countDown < 11; countDown++) {
+  console.log(countDown);
+}
+}
